ce9d822214627e96423c86546923c6644f60aaa6
Barndude <bwu22@stevens.edu>
2023-02-18 13:06:34 -0500

    small error

---
+++ client/src/components/AlertDialog/AlertDialogues.jsx
@@ -0,0 +1,43 @@
+import { useDisclosure } from "@chakra-ui/react";
+import { useRef } from "react";
+
+function AlertDialog() {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const cancelRef = React.useRef();
+	return (
+		<>
+			<Button colorScheme="red" onClick={onOpen}>
+				Delete Customer
+			</Button>
+
+			<AlertDialog
+				isOpen={isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={onClose}
+			>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize="lg" fontWeight="bold">
+							Delete Customer
+						</AlertDialogHeader>
+
+						<AlertDialogBody>
+							Are you sure? You can't undo this action afterwards.
+						</AlertDialogBody>
+
+						<AlertDialogFooter>
+							<Button ref={cancelRef} onClick={onClose}>
+								Cancel
+							</Button>
+							<Button colorScheme="red" onClick={onClose} ml={3}>
+								Delete
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+		</>
+	);
+}
+
+export default AlertDialog;
