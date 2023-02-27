125f8530d2441192fffd904ad9cde9caaf56fe0b
Barndude <bwu22@stevens.edu>
2023-02-27 10:08:28 -0500

    azure

--- client/src/components/SocketContext.jsx
+++ client/src/components/SocketContext.jsx
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io("https://simple-kanban-379104.uc.r.appspot.com/");
+export const socket = io("https://simple-kanban-379104.uc.r.appspot.com");
 export const SocketContext = createContext(socket);
 export default SocketContext;
