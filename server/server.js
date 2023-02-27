a529c132bb98f7f13fda3cead614d20f1fb02d4e
Barndude <bwu22@stevens.edu>
2023-02-26 20:40:15 -0500

    cors fix?

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ app.use(express.json());
 app.use(express.static("../client/public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+	res.header(
+		"Access-Control-Allow-Origin",
+		"https://simplekanbanboardio.netlify.app"
+	); // update to match the domain you will make the request from
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 app.use(
 	cors({
 		origin: true,
